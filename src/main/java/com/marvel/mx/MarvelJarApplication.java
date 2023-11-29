fb1cc51a4894da7d763fdc86a594d5ae2d32b3e9
Santiago Beltran <jonathan.beltran.ramirez@gmail.com>
2023-11-29 01:34:21 -0600

    initial commit

---
+++ src/main/java/com/marvel/mx/MarvelJarApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvel/mx/MarvelJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvel.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
